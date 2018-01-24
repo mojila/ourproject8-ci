7417123589c3264bfa72ca986e92bfbd9812d468
mojila <mojila@github.com>
2018-01-25 05:58:05 +0700

    /product

--- application/views/product.php
+++ application/views/product.php
@@ -1,124 +1,88 @@
-    <div class="page-wrap">
-        <!-- Heros-->
-        <div class="ps-section--hero pt-100 pb-100">
-            <div class="ps-section__content text-center">
-                <h3 class="ps-section__title">OUR PROJECT 8</h3>
-                <div class="ps-breadcrumb">
-                    <ol class="breadcrumb">
-                        <li><a href="<?php echo base_url('home'); ?>">Home</a></li>
-                        <li class="active">Product</li>
-                    </ol>
-                </div>
+<div class="ps-section--hero pb-10">
+  <img src="<?php echo base_url('dist/img/hero/01.jpg'); ?>" alt="">
+  <div class="ps-section__content text-center">
+    <h3 class="ps-section__title">OURPROJECT8</h3>
+    <div class="ps-breadcrumb">
+      <ol class="breadcrumb">
+        <li><a href="#">Home</a></li>
+        <li class="active">Product</li>
+      </ol>
+    </div>
+  </div>
+</div>
+<!-- Page Content -->
+<div class="ps-section--page">
+  <div class="container">
+    <div class="row">
+      <!-- Main -->
+      <div class="col col-lg-9 col-md-9 col-sm-12 col-xs-12 col-lg-push-3 col-md-push-3">
+        <div class="ps-shop-listing pt-80 pb-40">
+          <!-- Fitur -->
+          <div class="row">
+            <div class="col col-lg-4 col-md-4 col-sm-6 col-xs-12">
+              <img src="<?php echo base_url('dist/img/product-banner/012x.jpg'); ?>" alt="" class="mb-30">
             </div>
-        </div>
-        <div class="ps-section--page">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 col-lg-push-3 col-md-push-3">
-                        <div class="ps-shop-listing pt-50 pb-40">
-                            <div class="ps-shop">
-                            <div class="ps-product--list ps-product--list-large">
-                                    <?php
-                                        if(isset($jumlah)) {
-                                            if($jumlah == 0) {
-                                                echo '<script>
-                                                    alert("Tidak Ditemukan");
-                                                    window.location = "'.base_url('home/product').'";
-                                                </script>';
-                                            } else {
-                                                foreach($product as $p) {
-                                                    echo '
-                                                    <div class="ps-product__thumbnail">
-                                                    <a class="ps-product__overlay" href="'.base_url('home/product_detail/').$p->kode.'"></a>
-                                                    ';
-                                                    foreach($gambar_produk as $g) {
-                                                        if($g->kode_produk == $p->kode) {
-                                                            if($g->urutan == 1) {
-                                                                echo '<img src="'.base_url('dist/img/').$g->file.'" alt="'.$p->nama.'" class="ps-product__image">';
-                                                            }
-                                                        }
-                                                    }
-                                                    echo '
-                                                    </div>
-                                                    <div class="ps-product__content">
-                                                    <h4 class="ps-product__title"><a href="'.base_url('home/product_detail/').$p->kode.'">'.$p->nama.'</a></h4>
-                                                    <select class="ps-rating">
-                                                    ';
-                                                    $bintang_off = 5;
-                                                    $bintang_on = $p->bintang;
-                                                    $bintang_sisa = $bintang_off - $bintang_on;
-                                                    for($b = 1; $b <= $bintang_on; $b++){
-                                                        echo '<option value="1">'.$b.'</option>';
-                                                    }
-                                                    if($bintang_sisa != 0){
-                                                        for($c = $bintang_sisa; $c <= $bintang_off; $c++) {
-                                                            echo '<option value="5">'.$c.'</option>';
-                                                        }
-                                                    }
-                                                    echo '
-                                                        </select>
-                                                            <p class="ps-product__price">
-                                                                Keterangan
-                                                            </p>
-                                                            '.$p->keterangan.'
-                                                            <footer class="ps-product__footer clearfix">
-                                                                <div class="row">
-                                                                    <div class="col col-md-12 col-xs-12">
-                                                                        <a class="ps-btn ps-btn--sm" href="'.base_url('home/product_detail/').$p->kode.'">Lihat Detail<i class="fa fa-angle-right"></i></a>
-                                                                        <a class="ps-btn ps-btn--sm" style="background: #00B833 !important;" href="https://line.me/R/ti/p/%40ourproject8"><img src="https://media.line.me/img/web/lineit_select_line_icon_02.png" alt="" height="16"> Line</a>
-                                                                        <a class="ps-btn ps-btn--sm" style="background: #00E676 !important;" href="https://api.whatsapp.com/send?phone=6281249647753&amp;text=Saya%20Mau%20Order%20|%20'.base_url('home/product_detail').$p->kode.'"><img src="https://cdn.iconverticons.com/files/png/1c3a93787b4867ca_256x256.png" alt="" height="16"> WA</a>
-                                                                    </div>
-                                                                </div>
-                                                            </footer>
-                                                        </div>
-                                                    </div>
-                                                    ';
-                                                }
-                                            }
-                                        }
-                                    ?>
-                            </div>
-                            <div class="ps-pagination">
-                                <ul class="pagination">
-                                    <li><a href="#"><i class="fa fa-arrow-left"></i></a></li>
-                                    <li class="active"><a href="#">1</a></li>
-                                    <li><a href="#">2</a></li>
-                                    <li><a href="#">3</a></li>
-                                    <li><a href="#">4</a></li>
-                                    <li><a href="#"><i class="fa fa-arrow-right"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 col-lg-pull-9 col-md-pull-9">
-                        <div class="ps-sidebar">
-                            <aside class="ps-widget ps-widget--sidebar ps-widget--search">
-                                <form method="post" action="<?php echo base_url('home/search'); ?>">
-                                    <input name="keyword" class="form-control" type="text" placeholder="Ketikkan nama produk">
-                                    <button type="submit"><i class="ps-icon--search"></i></button>
-                                </form>
-                            </aside>
-                            <aside class="ps-widget ps-widget--sidebar ps-widget--best-seller">
-                                <div class="ps-widget__header">
-                                    <h3 class="ps-widget__title">CATEGORY</h3>
-                                </div>
-                                <div class="ps-widget__content">
-                                    <ul class="ps-list--circle">
-                                        <li class="current"><a href="<?php echo base_url('home/product'); ?>"><span class="circle"></span>Semua Produk</a></li>
-                                        <?php 
-                                            foreach($kategori as $k) {
-                                                echo '
-                                                    <li><a href="'.base_url('home/category/').$k->id.'"><span class="circle"></span>'.$k->nama.'</a></li>
-                                                ';
-                                            }
-                                        ?>
-                                    </ul>
-                                </div>
-                            </aside>
-                        </div>
-                    </div>
-                </div>
+            <div class="col col-lg-8 col-md-8 col-sm-6 col-xs-12">
+              <img src="<?php echo base_url('dist/img/product-banner/022x.jpg'); ?>" alt="" class="mb-30">
             </div>
+          </div>
+          <hr>
+          <div class="row">
+            <div class="ps-shop">
+              <!-- Item produk -->
+              <div class="ps-product--list ps-product--list-large">
+                <div class="ps-product__thumbnail">
+                  <a href="#" class="ps-product__overlay"></a>
+                  <img src="<?php echo base_url('dist/img/cake/img-cake-2.jpg'); ?>" alt="">
+                </div>
+                <div class="ps-product__content">
+                  <h4 class="ps-product__title pt-20">Red Sugar Flower</h4>
+                  <select class="ps-rating" name="">
+                    <option value="1">1</option>
+                    <option value="1">2</option>
+                    <option value="1">3</option>
+                    <option value="1">4</option>
+                    <option value="5">5</option>
+                  </select>
+                  <p class="ps-product__price">Keterangan</p>
+                  <p>Isi dari keterangan</p>
+                  <footer class="ps-product__footer clearfix">
+                    <a href="#" class="ps-btn ps-btn--sm">Detail <i class="fa fa-angle-right"></i></a>
+                    <ul class="ps-product__action">
+                      <li><a data-tooltip="Line" style="background: #00B833 !important;" href="https://line.me/R/ti/p/%40ourproject8"><img src="https://media.line.me/img/web/lineit_select_line_icon_02.png" alt="" height="32" style="margin-top: 0.65rem;"></a></li>
+                      <li><a data-tooltip="Whatsapp" style="background: #00E676 !important;" href="https://api.whatsapp.com/send?phone=6281249647753&amp;text=Saya%20Mau%20Order%20|%20'"><img src="https://cdn.iconverticons.com/files/png/1c3a93787b4867ca_256x256.png" alt="" height="32" style="margin-top: 0.65rem;"></a></li>
+                    </ul>
+                  </footer>
+                </div>
+              </div>
+              <hr>
+            </div>
+          </div>
         </div>
-        
-    
+      </div>
+      <!-- Sidebar -->
+      <div class="col col-lg-3 col-md-3 col-sm-12 col-xs-12 col-lg-pull-9 col-md-pull-9">
+        <div class="ps-sidebar">
+          <!-- Search -->
+          <aside class="ps-widget ps-widget--sidebar ps-widget--search">
+            <form class="" action="" method="post">
+              <input class="form-control" type="text" name="" value="" placeholder="Pencarian ...">
+              <button type="submit" name="button"><i class="ps-icon--search"></i></button>
+            </form>
+          </aside>
+          <aside class="ps-widget ps-widget--sidebar ps-widget--category">
+            <div class="ps-widget__header">
+                <h3 class="ps-widget__title">Kategori</h3>
+            </div>
+            <div class="ps-widget__content">
+              <ul class="ps-list--circle">
+                <li class="current"><a href="#"><span class="circle"></span> Semua Produk (100)</a></li>
+                <li><a href="#"><span class="circle"></span> Stiker (100)</a></li>
+              </ul>
+            </div>
+          </aside>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
